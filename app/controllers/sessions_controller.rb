5a5d1d7c0745433ee4911a4a8524e98dd0eec4fc
Btate712 <btate712@gmail.com>
2019-05-19 13:45:00 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name] == ""
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.delete(:name)
+    end
+  end
+
+end
