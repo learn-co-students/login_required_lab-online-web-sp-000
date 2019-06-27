6c5b98fdff726c1b7f0460d9c530f960c25eefb5
blainelawson <blaine.lawson@gmail.com>
2019-06-27 10:36:35 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-  
+
   def current_user
     session[:name]
+  end
+
+  def require_login
+    if !session.include?(:name)
+      redirect_to '/'
+    end
   end
 end
--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,6 @@
 class SecretsController < ApplicationController
+    before_action :require_login
+
     def show
-        if current_user.nil?
-            redirect_to '/'
-        end
     end
 end
