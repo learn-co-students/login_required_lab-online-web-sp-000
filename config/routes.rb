f5d8680dfc7900c0ac037aa8b58db58a6af36f94
blainelawson <blaine.lawson@gmail.com>
2019-06-27 09:12:33 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+  
+  get '/secret', to: 'secrets#show'
+  resources :secrets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
