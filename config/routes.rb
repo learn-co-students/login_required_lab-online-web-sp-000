82f8ec38f8406c1c41a72d4df6dda050bf1b3b9e
davisjustinw <davisjustinw@gmail.com>
2019-09-16 20:57:15 -0700

    all tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'application#index'
+  get '/secrets', to: 'secrets#show'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
