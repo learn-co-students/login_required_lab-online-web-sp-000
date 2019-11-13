eed5ab47b4eb64815a60bd27ebf32a6b83442d57
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-11-12 17:49:13 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'sessions#new'
   get '/sessions/new', to: "sessions#new"
   get '/secret', to: 'secrets#show'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  post '/sessions/login', to: 'sessions#create'
+  post '/sessions/logout', to: 'sessions#destroy'
 
 end
