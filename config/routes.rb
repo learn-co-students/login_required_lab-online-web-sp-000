b81beb3cd31cc8cbb23c1b487c7cab63a14437fd
dayofgray <graysonpberger@gmail.com>
2020-06-07 22:05:28 -0400

    Revert "Done."
    This reverts commit efdcf79bdadfe18168f7950cdd5f8da6bf8a7821.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'application#hello'
-
-  resources :sessions
-  resources :secrets
-
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
-
-  get '/secret' => 'secrets#show'
-
 end
