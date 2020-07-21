2a4f332c33b9d232e7c8d9118d49cf67023368a8
mbade1 <mbade314@gmail.com>
2020-07-21 11:39:33 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,13 @@
+class SecretsController < ApplicationController
+    before_action :require_login
+    
+    def show
+
+    end
+
+    private 
+    def require_login
+        redirect_to :login if current_user.nil? or current_user == ""
+    end
+
+end
