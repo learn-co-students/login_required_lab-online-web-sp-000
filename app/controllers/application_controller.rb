62f7337f7c998f3f915a03a47f1d7f2bb799f041
wkdewey <wkdewey@gmail.com>
2020-07-26 18:17:23 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   end
 
   def require_login
-    return head(:forbidden) unless session.include? :user_id
+    redirect_to new_session_path unless session.include? :name
   end
 end
