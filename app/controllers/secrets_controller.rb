0b3968adf230bfa6c044085b3a1fe62e2ab55bdc
Yumacorn <ReachRyanMui@gmail.com>
2020-10-07 22:00:21 -0400

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,13 +1,16 @@
 class SecretsController < ApplicationController
   before_action :require_login
 
-  def new
-  end
-
   def show
+    
   end
 
+  def destroy
+  end
+
+  private
   def require_login
-    return head(:forbidden) unless session.include? :user_id
+    # return head(:forbidden) unless session.include? :user_id
+    redirect_to '/login' if current_user == nil
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name] == nil || ''
+    if params[:name] == nil || params[:name] == ''
       redirect_to '/login'
     else
       session[:name] = params[:name]
+      redirect_to '/'
     end
   end
 
   def destroy
+    session.delete :name
+    redirect_to '/'
   end
 
 
   private
 
   def current_user
-    session[:name]
+    session[:username]
   end
 end
