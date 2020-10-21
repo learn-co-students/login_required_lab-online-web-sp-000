bcdcedcde7698f11f54d323fde2165d9f6647ef7
NicholasR77 <nl7robbins@icloud.com>
2020-10-20 19:56:44 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    session[:name] = params[:name]
+    redirect '/'
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
