3c67afb6313d86ab3c7f9fde07c2413b0b97d2de
David Strauch <dmaster18@gmail.com>
2021-01-24 19:12:35 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
+  resources :sessions, only: [:new, :create]
+
+  post '/sessions', to: 'sessions#destroy'
+
+  resources :secrets, only: [:index]
+
+  get '/secrets/show', to: 'secrets#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
