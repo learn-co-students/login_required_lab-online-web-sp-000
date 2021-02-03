ff49384afa8e10af8554bc0daf75b5f30bace311
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-03 16:33:32 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+    def home
+
+    end
+
+    def new
+
+    end
+
+    def create
+        if params[:name].nil? || params[:name].empty?
+            redirect_to controller: 'sessions', action: 'new'
+        else
+            session[:name] = params[:name]
+            redirect_to controller: 'application', action: 'hello'
+        end
+    end
+
+    def destroy
+        session.delete(:name) if session[:name]
+    end
+
+end
