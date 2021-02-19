ffc929ead43efe799cdbc2e1b5d572e6ab5bbef1
Ryan Burns <ryankburns@gmail.com>
2021-02-18 18:55:20 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    if session[:name]
+      redirect_to :root
+    else
+      redirect_to :login
+    end
+  end
+
   def current_user
     session[:name]
   end
