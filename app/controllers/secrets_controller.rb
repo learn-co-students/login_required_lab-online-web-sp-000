9b539203f39e92b254b8f120ecca58f1dbbf6811
taroj khan <tarujkhan@yahoo.com>
2021-03-10 02:32:07 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,15 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+  def show
+    if !current_user
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      redirect_to controller: 'secrets', action: 'show'
+  end
+end
+
+  private
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
+end
