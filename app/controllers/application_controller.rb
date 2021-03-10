16ebb05e489eb1ea1b49dbbf0ba5e96d550083a9
taroj khan <tarujkhan@yahoo.com>
2021-03-10 17:02:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user #||= User.find(session[:name]) if session[:name]
-end
+    @current_user = session[:name]
+  end
 
-def hello
-  redirect_to controller: 'sessions', action: 'new' unless session[:name]
-end
+  def hello
+    redirect_to controller: 'sessions', action: 'new' unless session[:name]
+  end
+
+  def require_login
+    #return head(:forbidden) unless session.include? :name if
+    redirect_to controller: 'sessions', action: 'new' unless session[:name]
+  end
 end
