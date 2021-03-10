53f7383d0d61e6c2bac740e9a9bb8e747a922a00
RSigmaSG <sg.comparch@gmail.com>
2021-03-09 21:26:15 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,11 @@
+class SecretsController < ApplicationController
+    before_action :require_login
+    def show
+
+    end
+    def require_login
+        if !session.include? :name
+            redirect_to '/login'
+        end
+    end
+end
