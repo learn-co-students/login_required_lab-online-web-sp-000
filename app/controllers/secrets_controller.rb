d9922e14469f0571fcfad4e47e2f4ff65d3144cf
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-05-27 10:06:47 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,7 @@
+class SecretsController < ApplicationController
+    before_action :current_user
+
+    def show
+
+    end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+    before_action :current_user
+
+    def create
+        byebug
+    end
+
+    def destroy
+
+    end
+end
